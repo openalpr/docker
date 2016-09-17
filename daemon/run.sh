d31f73ee829553925f8220b8181b09ee3d08d817
Matt Hill <matthill@openalpr.com>
2016-09-17 10:28:58 -0400

    Updated run script

--- daemon/run.sh
+++ daemon/run.sh
@@ -2,4 +2,4 @@
 
 LICENSE_KEY="licensekeygoeshere"
 
-docker run -P -e OPENALPR_LICENSE_KEY=$LICENSE_KEY -v agentconfig:/etc/openalpr/ -v agentruntime:/usr/share/openalpr/ -v /tmp/plateimages:/var/lib/openalpr/plateimages/ -it openalpr/commercial-agent 
+docker run -P -e OPENALPR_LICENSE_KEY=$LICENSE_KEY -v openalpr-vol1:/etc/openalpr/ -v openalpr-vol1:/usr/share/openalpr/install_id -v openalpr-vol1:/var/lib/openalpr/plateimages/ -it openalpr/commercial-agent alprlink-register
